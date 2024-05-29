87f313dd6dc45f183535f3e87e8417899c5e8436
mathiashole <joacomangino@gmail.com>
2024-05-29 20:40:52 -0300

    we removed comments of get_time_v1 program

--- codigo/get_time_v1.R
+++ codigo/get_time_v1.R
@@ -10,45 +10,6 @@ if (length(args) == 0) {
 library(stringr)
 library(lubridate)
 
-# # Función para buscar la fecha en un archivo
-# find_date_in_file <- function(file_path) {
-#   # Patrones de fecha en español e inglés
-#   #date_pattern_es <- "\\b(?:\\d{1,2}[\\/\\-]\\d{1,2}[\\/\\-](?:\\d{2})?\\d{2}|\\d{1,2}\\s+de\\s+(?:enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|setiembre|octubre|noviembre|diciembre)\\s+de\\s+(?:\\d{2})?\\d{2})\\b"
-#   date_pattern_es <- "\\b(?:\\d{1,2}\\s+(?:de\\s+)?(?:enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|setiembre|octubre|noviembre|diciembre)\\s+(?:de\\s+)?(?:\\d{2})?\\d{2}|\\d{1,2}[\\/\\-]\\d{1,2}[\\/\\-](?:\\d{2})?\\d{2})\\b"
-#   date_pattern_en <- "\\b(?:\\d{1,2}[\\/\\-]\\d{1,2}[\\/\\-](?:\\d{2})?\\d{2}|\\d{1,2}\\s+(?:January|February|March|April|May|June|July|August|September|October|November|December)\\s+\\d{4})\\b"
-
-#   lines <- readLines(file_path, n = 10)
-  
-#   date_found <- NA
-
-#   # Buscar fechas en las primeras 10 líneas del archivo
-#   for (line in lines) {
-#     date_in_line_es <- str_extract(line, date_pattern_es)
-#     date_in_line_en <- str_extract(line, date_pattern_en)
-    
-#     if (!is.na(date_in_line_es)) {
-#       if (grepl("\\d{1,2}[\\/\\-]\\d{1,2}[\\/\\-](?:\\d{2})?\\d{2}", date_in_line_es)) {
-#         parsed_date <- dmy(date_in_line_es, quiet = TRUE)
-#       } else {
-#         Sys.setlocale("LC_TIME", "es_ES.UTF-8")
-#         parsed_date <- dmy(date_in_line_es, quiet = TRUE)
-#       }
-#       if (!is.na(parsed_date)) {
-#         date_found <- format(parsed_date, "%d/%m/%Y")
-#         break
-#       }
-#     } else if (!is.na(date_in_line_en)) {
-#       parsed_date <- mdy(date_in_line_en, quiet = TRUE)
-#       if (!is.na(parsed_date)) {
-#         date_found <- format(parsed_date, "%d/%m/%Y")
-#         break
-#       }
-#     }
-#   }
-
-#   return(ifelse(is.na(date_found), "Fecha no encontrada", date_found))
-# }
-
 # Función para buscar la fecha en un archivo
 find_date_in_file <- function(file_path) {
   # Patrones de fecha en español e inglés
