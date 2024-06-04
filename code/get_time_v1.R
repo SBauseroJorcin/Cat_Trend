ad4002264ff51e155a9e2264bdce405fe8f154a6
mathiashole <joacomangino@gmail.com>
2024-06-04 19:45:55 -0300

    we changed all comments

--- code/get_time_v1.R
+++ code/get_time_v1.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-# Obtener los argumentos de línea de comandos
+# Get the command line arguments
 args <- commandArgs(trailingOnly = TRUE)
 
 if (length(args) == 0) {
@@ -10,9 +10,9 @@ if (length(args) == 0) {
 library(stringr)
 library(lubridate)
 
-# Función para buscar la fecha en un archivo
+# Function to search the date in a file
 find_date_in_file <- function(file_path) {
-  # Patrones de fecha en español e inglés
+  # Date patterns in Spanish and English
   date_pattern_es <- "\\b(?:\\d{1,2}\\s+(?:de\\s+)?(?:enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|setiembre|octubre|noviembre|diciembre)\\s+(?:de\\s+)?(?:\\d{2})?\\d{2}|\\d{1,2}[\\/\\-]\\d{1,2}[\\/\\-](?:\\d{2})?\\d{2})\\b"
   #date_pattern_en <- "\\b(?:\\d{1,2}\\s+(?:January|February|March|April|May|June|July|August|September|October|November|December)\\s+\\d{4})\\b"
   date_pattern_en <- "\\b(?:\\d{1,2}\\s+(?:January|February|March|April|May|June|July|August|September|October|November|December),?\\s+(?:\\d{2})?\\d{2})\\b|\\b(?:January|February|March|April|May|June|July|August|September|October|November|December)\\s+\\d{1,2},?\\s+(?:\\d{2})?\\d{2}\\b|\\b\\d{1,2}-(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-\\d{2,4}\\b|\\b\\d{1,2}[\\/\\-]\\d{1,2}[\\/\\-](?:\\d{2})?\\d{2}\\b"
@@ -22,18 +22,18 @@ find_date_in_file <- function(file_path) {
   
   date_found <- NA
 
-  # Buscar fechas en las primeras 10 líneas del archivo
+  # Find dates in the first 10 lines of the file
   for (line in lines) {
-    # Limpiar la línea eliminando las comas pegadas a los números y agregando un espacio después de la coma
+    # Clean the line by removing commas attached to the numbers and adding a space after the comma
     line_cleaned <- gsub("(?<=[0-9])\\,|\\,(?=[0-9])", ", ", line, perl = TRUE)
     
-    # Luego, limpiar los dos puntos pegados a letras o palabras (no números)
+    # Then, clean the colons attached to letters or words (not numbers)
     line_cleaned <- gsub("(?<=[A-Za-z])\\:+|\\:+(?=[A-Za-z])", "", line_cleaned, perl = TRUE)
     
-    # Agregar un espacio después de eliminar los dos puntos
+    # Add a space after removing the colon
     line_cleaned <- gsub(":", ": ", line_cleaned)
     
-    # Eliminar los "de" que están entre una palabra y un número, pero no entre un número y una palabra
+    # Remove "de"s that are between a word and a number, but not between a number and a word
     line_cleaned <- gsub("(?<=\\D)\\bde\\b(?=\\d)", "", line_cleaned, perl = TRUE)
     
     date_in_line_es <- str_extract(line_cleaned, date_pattern_es)
@@ -57,8 +57,7 @@ find_date_in_file <- function(file_path) {
   return(ifelse(is.na(date_found), "Fecha no encontrada", date_found))
 }
 
-
-# Función para generar la tabla de datos
+# Function to generate the data table
 generate_table <- function(dir_path) {
   files <- list.files(path = dir_path, pattern = "\\.txt$", full.names = TRUE)
   table <- sapply(files, function(file) {
@@ -68,20 +67,20 @@ generate_table <- function(dir_path) {
   return(t(table))
 }
 
-# Directorio que contiene los archivos txt
+# Directory containing the txt files
 directorio <- args[1]
 
-# Verificar si el directorio existe
+# Check if the directory exists
 if (!file.exists(directorio)) {
   stop("El directorio especificado no existe.")
 }
 
-# Verificar si hay archivos .txt en el directorio
+# Check if there are .txt files in the directory
 if (length(list.files(directorio, pattern = "\\.txt$", full.names = TRUE)) == 0) {
   stop("No se encontraron archivos .txt en el directorio especificado.")
 }
 
-# Generar y guardar la tabla de datos
+# Generate and save data table
 tabla_datos <- generate_table(directorio)
 write.table(tabla_datos, file = "tabla_datos.txt", sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
