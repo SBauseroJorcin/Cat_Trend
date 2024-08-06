89374c5d97fa7725d5d7ae15edad2b6b656595a5
mathiashole <joacomangino@gmail.com>
2024-08-06 15:58:43 -0300

    changed name again

--- code/txt_frequency.R
+++ code/txt_frequency.R
@@ -57,8 +57,8 @@ analyze_frequency <- function(date_hour) {
   tf_idf_plot <- plot_tf_idf(docs_words, title="Top 10 Words by TF-IDF", xlab=NULL, ylab="TF-IDF")
   save_plot_to_pdf(tf_idf_plot, paste0("output/frequency/tf_idf_", date_hour, ".pdf"))
 
-  # # Plot word by year
-  # word_per_year_plot <- plot_words_per_year(data_words, title="Frequency Distribution of Terms by Year", xlab="Year", ylab="Total Words")
-  # save_plot_to_pdf(year_frequency_plot, paste0("output/frequency/total_word_by_year", date_hour, ".pdf"))
+  # Plot word by year
+  word_per_year_plot <- plot_words_per_year(data_words, title="Frequency Distribution of Terms by Year", xlab="Year", ylab="Total Words")
+  save_plot_to_pdf(word_per_year_plot, paste0("output/frequency/total_word_by_year", date_hour, ".pdf"))
 
 }
